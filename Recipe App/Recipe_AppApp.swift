75f0611d5fbd6b1aa9356be879acd509bbc539b8
raduliano <radulian@me.com>
2022-03-22 12:49:38 +0200

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Octav Radulian on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
